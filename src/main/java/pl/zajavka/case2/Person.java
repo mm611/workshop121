d7cf18acc6ea4a786265a19fbe2d5d8de9bb99c8
Marciniszyn Koleszka <testowykoleszka@zajavka.pl>
2023-10-05 22:25:35 +0200

    Person class modification 1

--- src/main/java/pl/zajavka/case2/Person.java
+++ src/main/java/pl/zajavka/case2/Person.java
@@ -2,6 +2,7 @@ package pl.zajavka.case2;
 
 import java.math.BigDecimal;
 
+
 record Person(String name, BigDecimal salary) {
 
     public BigDecimal howMuchDoYouEarn() {
@@ -11,4 +12,16 @@ record Person(String name, BigDecimal salary) {
     public Boolean doYouLikePizza() {
         return true;
     }
+    public Boolean doYouHaveABike() {
+        return true;
+    }
+
+    public String whatIsYourJob() {
+        return "Software developer!";
+    }
+
+    public String whatDoYouLike() {
+        return "Strawberries";
+    }
+
 }
